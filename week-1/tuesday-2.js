51fa109d2354571457a27e540fe76cc13d85b957
Nina Vens <nina.vens@hva.nl>
2023-10-16 11:54:04 +0200

    added: tue exercise 2

---
+++ week-1/tuesday-2.js
@@ -0,0 +1,11 @@
+let data = ['robert', 'vincent', 'lAuRa', 'Cas', 'wIMER', 'rOOs'];
+
+function convertArrayStringsToCapitalized() {
+	data = data.map((item) => {
+		return item.toLowerCase().charAt(0).toUpperCase() + item.slice(1).toLowerCase();
+	});
+
+	console.log(data);
+}
+
+convertArrayStringsToCapitalized();
