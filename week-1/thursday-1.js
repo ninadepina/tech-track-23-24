a26eb0a9dbc66544e479def8a74870b1003802fc
Nina Vens <nina.vens@hva.nl>
2023-10-18 14:32:03 +0200

    added: thu exercise 1

---
+++ week-1/thursday-1.js
@@ -0,0 +1,42 @@
+const data = [
+	{
+		id: 1,
+		name: 'Robert',
+		kaas: true
+	},
+	{
+		id: 2,
+		name: 'Vincent',
+		kaas: false
+	},
+	{
+		id: 3,
+		name: 'Laura',
+		kaas: true
+	}
+];
+
+function generateTable() {
+	// const table = document.querySelector('table');
+	const thead = document.querySelector('thead');
+	const tbody = document.querySelector('tbody');
+
+	Object.keys(data[0]).forEach((key) => {
+		const th = document.createElement('th');
+		th.innerText = key;
+		thead.appendChild(th);
+	});
+
+	data.forEach((item) => {
+		const tr = document.createElement('tr');
+		tbody.appendChild(tr);
+
+		for (const [key, value] of Object.entries(item)) {
+			const td = document.createElement('td');
+			td.textContent = value;
+			tr.appendChild(td);
+		}
+	});
+}
+
+generateTable();
