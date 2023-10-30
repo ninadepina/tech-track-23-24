3cf232b7dc9eed3a17001539291c9a826313bfba
Nina Vens <nina.vens@hva.nl>
2023-10-30 22:05:30 +0100

    changed: commented out header/footer

--- week-2/src/routes/+layout.svelte
+++ week-2/src/routes/+layout.svelte
@@ -1,8 +1,9 @@
 <script>
-	import Header from '$lib/header.svelte';
-	import Footer from '$lib/footer.svelte';
+	import '../app.css';
+	// import Header from '$lib/header.svelte';
+	// import Footer from '$lib/footer.svelte';
 </script>
 
-<Header />
+<!-- <Header /> -->
 <slot />
-<Footer />
+<!-- <Footer /> -->
