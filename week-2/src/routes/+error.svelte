ba1c852d946ba8a5a99ab936b3f1feaa382d1f29
Nina Vens <nina.vens@hva.nl>
2023-10-30 11:19:07 +0100

    added: boilerplate

---
+++ week-2/src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<main>
+	<!-- <img src="/error.png" alt="404 error message" /> -->
+</main>
+
+<style>
+	main {
+		padding: 0;
+	}
+	img {
+		width: 100%;
+	}
+</style>
---
+++ week-2/src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import LinePlot from '$lib/LinePlot.svelte';
+</script>
+
+<svelte:head>
+	<title>ninadepina</title>
+	<link rel="icon" href="/images/favicon.png" type="image/png" />
+	<link rel="stylesheet" href="style.css" />
+</svelte:head>
+
+<h1>ninadepina</h1>
+
+<LinePlot />
