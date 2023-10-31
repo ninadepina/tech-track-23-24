b083f0d6076df29c2b9078d099aba6c2db551621
Nina Vens <nina.vens@hva.nl>
2023-10-31 12:06:32 +0100

    changed: <main>

--- week-2/src/routes/+error.svelte
+++ week-2/src/routes/+error.svelte
@@ -1,4 +1,2 @@
-<main>
-    <h1>404</h1>
-    <p>Page not found</p>
-</main>
+<h1>404</h1>
+<p>Page not found</p>
--- week-2/src/routes/+layout.svelte
+++ week-2/src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script>
-	import '../app.css';
-	// import Header from '$lib/header.svelte';
-	// import Footer from '$lib/footer.svelte';
+    import '../app.css';
+    // import Header from '$lib/header.svelte';
+    // import Footer from '$lib/footer.svelte';
 </script>
 
-<!-- <Header /> -->
-<slot />
-<!-- <Footer /> -->
+<main>
+    <!-- <Header /> -->
+    <slot />
+    <!-- <Footer /> -->
+</main>
