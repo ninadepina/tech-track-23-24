51b06d67375a170283392fd4f1044149089c1981
Nina Vens <nina.vens@hva.nl>
2023-11-01 22:37:49 +0100

    removed: header/footer

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,11 +1,7 @@
 <script>
     import '../app.css';
-    // import Header from '$lib/header.svelte';
-    // import Footer from '$lib/footer.svelte';
 </script>
 
-<!-- <Header /> -->
 <main>
     <slot />
 </main>
-<!-- <Footer /> -->
