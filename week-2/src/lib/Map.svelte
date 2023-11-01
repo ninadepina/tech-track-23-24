2472d3efe84a7694fbc7caf086a4c33be964ab7b
Nina Vens <nina.vens@hva.nl>
2023-11-01 14:23:52 +0100

    added: map component

---
+++ week-2/src/lib/Map.svelte
@@ -0,0 +1,26 @@
+<script>
+    import { onMount } from 'svelte';
+    import mapboxgl from 'mapbox-gl';
+    import { initializeMap } from '$lib/mapbox.js';
+
+    let map;
+
+    onMount(() => {
+        map = initializeMap();
+
+        map.addControl(new mapboxgl.NavigationControl());
+        map.addControl(new mapboxgl.FullscreenControl());
+    });
+</script>
+
+<div id="map" />
+
+<style>
+    #map {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        width: 100%;
+        z-index: 0;
+    }
+</style>
--- week-2/src/routes/schiphol/+page.svelte
+++ week-2/src/routes/schiphol/+page.svelte
@@ -1,16 +1,5 @@
 <script>
-    import { onMount } from 'svelte';
-    import mapboxgl from 'mapbox-gl';
-    import { initializeMap } from '$lib/mapbox.js';
-
-    let map;
-
-    onMount(() => {
-        map = initializeMap();
-
-        map.addControl(new mapboxgl.NavigationControl());
-        map.addControl(new mapboxgl.FullscreenControl());
-    });
+    import Map from '$lib/Map.svelte';
 </script>
 
 <svelte:head>
@@ -20,19 +9,11 @@
 </svelte:head>
 
 <h1>Schiphol</h1>
-<div id="map" />
+<Map />
 
 <style>
     h1 {
         position: relative;
         z-index: 1;
-    }
-
-    #map {
-        position: absolute;
-        top: 0;
-        bottom: 0;
-        width: 100%;
-        z-index: 0;
     }
 </style>
